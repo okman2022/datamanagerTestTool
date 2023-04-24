76d673a3b59706396680a2421a35383ab061b8c4
okman2022 <114091223+okman2022@users.noreply.github.com>
2023-04-24 11:06:57 +0800

    Create define.h
    test tool define

---
+++ define.h
@@ -0,0 +1,6 @@
+//add by okman 2023.04.24 for testtool about datamanager gui
+
+#define TOOLNAME "datamanagerTestTool"
+#define QT_VER     "Qt5.3"
+#define datamanager_Server_ip "198.112.0.10"
+#define datamanager_Server_port 208713
